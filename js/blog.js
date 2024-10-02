738f02a0f783d998a509954ae0874118d3869d95
SparkLineLabs <anwaarsoomro50@gmail.com@example.com>
2024-10-02 16:02:11 +0500

    blog.html and indexafter.html page are updated 😊😊

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+const blogPosts = document.getElementById('blogPosts');
+
+        window.onload = () => {
+            blogPosts.style.opacity = '1'; // Fade in effect
+            blogPosts.style.transform = 'translateY(0)'; // Move into view
+        };
