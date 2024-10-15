f906c6352456a17f9dad858c89cc49748d2927e9
SparkLineLabs <anwaarsoomro50@gmail.com@example.com>
2024-10-15 18:22:12 +0500

    .

--- js/events.js
+++ js/events.js
@@ -4,9 +4,7 @@ const eventCards = document.querySelectorAll('.event-card');
 eventCards.forEach(card => {
     card.addEventListener('mouseover', () => {
         const img = card.querySelector('img');
-        const eventtext = document.querySelectorAll('.event-title,.event-category,.event-card-part,.event-date');
         img.style.opacity = '1'; 
-       eventtext.style.color = '#fff';
     });
 
     card.addEventListener('mouseout', () => {
